9ea9c9ead89e5de1e5fb671e71a559610bb1c793
Mateus Cerqueira Gonçalves <888388@sga.pucminas.br>
2018-11-08 21:07:40 -0200

    Atualizando DTO's

--- src/main/java/com/javaee/springjpamysql/api/v1/model/GasStationDTO.java
+++ src/main/java/com/javaee/springjpamysql/api/v1/model/GasStationDTO.java
@@ -1,5 +1,8 @@
 package com.javaee.springjpamysql.api.v1.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +12,7 @@ public class GasStationDTO {
 
 	private Long id;
 	
+	@NotBlank
+    @Size(min = 2, max = 255)
 	private String name;
 }
